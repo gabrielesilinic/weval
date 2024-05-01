f09d6f1f9990460f2cbee11f18cf971cb0499c3a
gabrielesilinic <gabrielesilinic@gmail.com>
2024-05-01 14:50:31 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,197 @@
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum TokenState {
+    None,
+    Number,
+    Operator,
+    Parenthesis,
+}
+
+fn is_operator(op: &str) -> bool {
+    match op {
+        "+" | "-" | "*" | "/" | "**" => true,
+        _ => false,
+    }
+}
+
+fn get_operator_precedence(op: &str) -> i32 {
+    match op {
+        "+" | "-" => 1,
+        "*" | "/" => 2,
+        "**" => 3,
+        _ => 0,
+    }
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn tokenizer(expr: String) -> Vec<String> {
+    /*
+    tokens are: floating point numbers, operators (+,-,*,/,**) and parentheses
+    */
+    let mut tokens: Vec<String> = Vec::new();
+    let mut num_buffer: String = String::new();
+    let mut current_state: TokenState = TokenState::None;
+    let mut past_state: TokenState;
+    let mut skip = 0;
+    // make it state machine style
+    for i in 0..expr.len() {
+        if skip > 0 {
+            skip -= 1;
+            continue;
+        }
+        let c = expr.chars().nth(i).unwrap();
+        past_state = current_state;
+        match c {
+            //match an operator
+            '+' | '-' | '*' | '/' => {
+                current_state = TokenState::Operator;
+                //it may be power operator **, check next character
+                if i < expr.len() - 1 && expr.chars().nth(i + 1).unwrap() == '*' {
+                    tokens.push(c.to_string() + "*");
+                    skip = 1;
+                } else {
+                    tokens.push(c.to_string());
+                }
+            }
+            //match a parenthesis
+            '(' | ')' => {
+                current_state = TokenState::Parenthesis;
+                tokens.push(c.to_string());
+            }
+            //match a number
+            '.' | '0'..='9' => {
+                current_state = TokenState::Number;
+                //if past state was operator, check if the token was + or -, then check if the token before it was also an operator,
+                //if so, merge the + or - with the number buffer and remove the last token
+                if past_state == TokenState::Operator
+                    || past_state == TokenState::Parenthesis && tokens.last().unwrap() == "("
+                {
+                    if tokens.last().unwrap() == "+" || tokens.last().unwrap() == "-" {
+                        //check if it is the first token ever, if it is, merge it with the number buffer then remove it
+                        if tokens.len() == 1 {
+                            num_buffer = tokens.pop().unwrap() + &num_buffer;
+                        }
+                        //otherwise check if there were more than 1 tokens, and if the one before was an operator
+                        else if tokens.len() > 1 {
+                            if is_operator(&tokens[tokens.len() - 2])
+                                || tokens[tokens.len() - 2] == "("
+                            {
+                                num_buffer = tokens.pop().unwrap() + &num_buffer;
+                            }
+                        }
+                    }
+                }
+                //iterate over the string and add each number character to the number buffer
+                let mut explored_digits_count = 0;
+                for j in i..expr.len() {
+                    let c2 = expr.chars().nth(j).unwrap();
+                    if c2 == '.' || c2.is_digit(10) {
+                        num_buffer.push(c2);
+                        explored_digits_count += 1;
+                    } else {
+                        break;
+                    }
+                }
+                tokens.push(num_buffer.clone());
+                num_buffer.clear();
+                skip = if explored_digits_count > 0 {
+                    explored_digits_count - 1
+                } else {
+                    explored_digits_count
+                };
+            }
+            //default case, do nothing
+            _ => {}
+        }
+    }
+    //merge + and - with numbers whenever an operator is followed by a number
+    //return the tokens
+    return tokens;
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn infix_to_postfix_tokens(tokens: Vec<String>) -> Vec<String> {
+    let mut output: Vec<String> = Vec::new();
+    let mut operators: Vec<String> = Vec::new();
+
+    for token in tokens {
+        match token.as_str() {
+            "(" => {
+                operators.push(token);
+            }
+            ")" => {
+                while operators.len() > 0 && operators[operators.len() - 1] != "(" {
+                    output.push(operators.pop().unwrap());
+                }
+                operators.pop();
+            }
+            "+" | "-" | "*" | "/" | "**" => {
+                //if the current token to operators only if it has a greater precedence compared to the last operator in the stack
+                while operators.len() > 0
+                    && get_operator_precedence(&token)
+                        <= get_operator_precedence(&operators[operators.len() - 1])
+                {
+                    output.push(operators.pop().unwrap());
+                }
+                operators.push(token);
+            }
+            _ => {
+                output.push(token);
+            }
+        }
+    }
+    while operators.len() > 0 {
+        output.push(operators.pop().unwrap());
+    }
+
+    output
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn eval_float64(expr: String) -> f64 {
+    let tokens = tokenizer(expr);
+    let postfix_tokens = infix_to_postfix_tokens(tokens);
+    let mut stack: Vec<f64> = Vec::new();
+    for token in postfix_tokens {
+        match token.as_str() {
+            "+" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a + b);
+            }
+            "-" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b - a);
+            }
+            "*" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a * b);
+            }
+            "/" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b / a);
+            }
+            "**" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b.powf(a));
+            }
+            _ => {
+                stack.push(token.parse::<f64>().unwrap());
+            }
+        }
+    }
+    stack.pop().unwrap()
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn eval_float64_str(expr: String) -> String {
+    eval_float64(expr).to_string()
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,332 @@
+use super::*;
+
+#[test]
+fn tokenizer_defaultok() {
+    let expr = "1+2+-5";
+    let tokens = tokenizer(expr.to_string());
+    assert_eq!(tokens, vec!["1", "+", "2", "+", "-5"]);
+}
+#[test]
+fn tokenizer_defaultok2() {
+    let expr = "1+2+-5*3";
+    let tokens = tokenizer(expr.to_string());
+    assert_eq!(tokens, vec!["1", "+", "2", "+", "-5", "*", "3"]);
+}
+#[test]
+fn tokenizer_defaultok3() {
+    let expr = "1+2+-5*3/4";
+    let tokens = tokenizer(expr.to_string());
+    assert_eq!(tokens, vec!["1", "+", "2", "+", "-5", "*", "3", "/", "4"]);
+}
+#[test]
+fn tokenizer_parerentheses() {
+    let expr = "+43**(1+2)**-23.45";
+    let tokens = tokenizer(expr.to_string());
+    assert_eq!(
+        tokens,
+        vec!["+43", "**", "(", "1", "+", "2", ")", "**", "-23.45"]
+    );
+}
+#[test]
+fn tokenizer_parerentheses2() {
+    let expr = "34+(1+2**3)*3";
+    let tokens = tokenizer(expr.to_string());
+    assert_eq!(
+        tokens,
+        vec!["34", "+", "(", "1", "+", "2", "**", "3", ")", "*", "3"]
+    );
+}
+#[test]
+fn tokenizer_parerentheses3() {
+    let expr = "34**(1+2**3)*-3/4";
+    let tokens = tokenizer(expr.to_string());
+    assert_eq!(
+        tokens,
+        vec!["34", "**", "(", "1", "+", "2", "**", "3", ")", "*", "-3", "/", "4"]
+    );
+}
+#[test]
+fn infix_to_postfix_tokens_defaultok() {
+    //1+2*(3-4)**2+5**(3+4+5)
+    //1 2 3 4 - 2 ** * + 5 3 4 + 5 + **
+    let tokens: Vec<String> = vec![
+        "1", "+", "2", "*", "(", "3", "-", "4", ")", "**", "2", "+", "5", "**", "(", "3", "+", "4",
+        "+", "5", ")",
+    ]
+    .iter()
+    .map(|&s| s.to_string())
+    .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(
+        postfix_tokens,
+        vec!["1", "2", "3", "4", "-", "2", "**", "*", "+", "5", "3", "4", "+", "5", "+", "**", "+"]
+    );
+}
+#[test]
+fn infix_to_postfix_tokens_sum() {
+    let tokens: Vec<String> = vec!["1", "+", "2"].iter().map(|&s| s.to_string()).collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sub() {
+    let tokens: Vec<String> = vec!["1", "-", "2"].iter().map(|&s| s.to_string()).collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "-"]);
+}
+#[test]
+fn infix_to_postfix_tokens_mul() {
+    let tokens: Vec<String> = vec!["1", "*", "2"].iter().map(|&s| s.to_string()).collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "*"]);
+}
+#[test]
+fn infix_to_postfix_tokens_div() {
+    let tokens: Vec<String> = vec!["1", "/", "2"].iter().map(|&s| s.to_string()).collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "/"]);
+}
+#[test]
+fn infix_to_postfix_tokens_pow() {
+    let tokens: Vec<String> = vec!["1", "**", "2"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "**"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sum_mul() {
+    let tokens: Vec<String> = vec!["1", "+", "2", "*", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "*", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_mul_sum() {
+    let tokens: Vec<String> = vec!["1", "*", "2", "+", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "*", "3", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sum_sub() {
+    let tokens: Vec<String> = vec!["1", "+", "2", "-", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "+", "3", "-"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sub_sum() {
+    let tokens: Vec<String> = vec!["1", "-", "2", "+", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "-", "3", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sum_div() {
+    let tokens: Vec<String> = vec!["1", "+", "2", "/", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "/", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_div_sum() {
+    let tokens: Vec<String> = vec!["1", "/", "2", "+", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "/", "3", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sum_pow() {
+    let tokens: Vec<String> = vec!["1", "+", "2", "**", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "**", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_pow_sum() {
+    let tokens: Vec<String> = vec!["1", "**", "2", "+", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "**", "3", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sub_mul() {
+    let tokens: Vec<String> = vec!["1", "-", "2", "*", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "*", "-"]);
+}
+#[test]
+fn infix_to_postfix_tokens_div_pow() {
+    let tokens: Vec<String> = vec!["1", "/", "2", "**", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "**", "/"]);
+}
+#[test]
+fn infix_to_postfix_tokens_pow_div() {
+    let tokens: Vec<String> = vec!["1", "**", "2", "/", "3"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "**", "3", "/"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sum_mul_div() {
+    let tokens: Vec<String> = vec!["1", "+", "2", "*", "3", "/", "4"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "*", "4", "/", "+"]);
+}
+#[test]
+fn infix_to_postfix_tokens_sum_div_mul() {
+    let tokens: Vec<String> = vec!["1", "+", "2", "/", "3", "*", "4"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
+    let postfix_tokens = infix_to_postfix_tokens(tokens.clone());
+    assert_eq!(postfix_tokens, vec!["1", "2", "3", "/", "4", "*", "+"]);
+}
+#[test]
+fn eval_float64_defaultok() {
+    let expr = "1+2*(3-4)**2+5**(3+4+5)";
+    let expected = 1.0 + 2.0 * (3.0f64 - 4.0f64).powf(2.0) + 5.0f64.powf(3.0 + 4.0 + 5.0);
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_sum() {
+    let expr = "1+2";
+    let expected = 1.0 + 2.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_sub() {
+    let expr = "1-2";
+    let expected = 1.0 - 2.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_mul() {
+    let expr = "1*2";
+    let expected = 1.0 * 2.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_div() {
+    let expr = "1/2";
+    let expected = 1.0 / 2.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_pow() {
+    let expr = "1**2";
+    let expected = 1.0f64.powf(2.0);
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_sum_mul() {
+    let expr = "1+2*3";
+    let expected = 1.0 + 2.0 * 3.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_mul_sum() {
+    let expr = "1*2+3";
+    let expected = 1.0 * 2.0 + 3.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_sum_sub() {
+    let expr = "1+2-3";
+    let expected = 1.0 + 2.0 - 3.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_mul_pow() {
+    let expr = "1.4*2**3";
+    let expected = 1.4 * 2.0f64.powf(3.0);
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_pow_sub() {
+    let expr = "1**2-3";
+    let expected = 1.0f64.powf(2.0) - 3.0;
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_complexok() {
+    let expr = "((3 + 5 * 2) / (4 - 7 * (2 + 3)) + 8) / (6 * (4 + 2 ** 3) - 9) + (5 ** 2 - (2 + 3 * (4 / 2)))";
+    let expected = ((3.0 + 5.0 * 2.0) / (4.0 - 7.0 * (2.0 + 3.0)) + 8.0)
+        / (6.0 * (4.0 + 2.0f64.powf(3.0)) - 9.0)
+        + (5.0f64.powf(2.0) - (2.0 + 3.0 * (4.0 / 2.0)));
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_complexok2() {
+    let expr = "((3 + 5 * 2) / (4 - 7 * (2 + 3)) + 8) / (6 * (4 + 2 ** 3) - 9) ** (5 ** 2 - (2 + 3 * (4 / 2)))";
+    let expected = ((3.0 + 5.0 * 2.0) / (4.0 - 7.0 * (2.0 + 3.0)) + 8.0)
+        / (6.0 * (4.0 + 2.0f64.powf(3.0)) - 9.0).powf(5.0f64.powf(2.0) - (2.0 + 3.0 * (4.0 / 2.0)));
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_floatcomplexok() {
+    let expr = "((3.5 + 5.2 * 2.3) / (4.1 - 7.9 * (2.8 + 3.7)) + 8.6) / (6.4 * (4.2 + 2.3 ** 3.1) - 9.7) ** (5.3 ** 2.2 - (2.1 + 3.4 * (4.2 / 2.3)))";
+    let expected = ((3.5 + 5.2 * 2.3) / (4.1 - 7.9 * (2.8 + 3.7)) + 8.6)
+        / (6.4 * (4.2 + 2.3f64.powf(3.1)) - 9.7).powf(5.3f64.powf(2.2) - (2.1 + 3.4 * (4.2 / 2.3)));
+    let result = eval_float64(expr.to_string());
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn eval_float64_str_defaultok() {
+    let expr = "1+2*(3-4)**2+5**(3+4+5)";
+    let expected =
+        (1.0 + 2.0 * (3.0f64 - 4.0f64).powf(2.0) + 5.0f64.powf(3.0 + 4.0 + 5.0)).to_string();
+    let result = eval_float64_str(expr.to_string());
+    assert_eq!(result, expected);
+}
+#[test]
+fn eval_float64_str_floatok() {
+    let expr = "54*0.7";
+    let expected = (54.0f64 * 0.7f64).to_string();
+    let result = eval_float64_str(expr.to_string());
+    assert_eq!(result, expected);
+}
